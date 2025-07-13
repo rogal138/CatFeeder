e1b92219c589fa3238f67fa1c1b0ee619d4d72ca
rogal138 <michal.r1993@gmail.com>
2025-07-13 22:42:44 +0200

    Added HAL

--- esphome/components/cat_feeder/cat_feeder.cpp
+++ esphome/components/cat_feeder/cat_feeder.cpp
@@ -8,7 +8,7 @@ static const char *TAG = "cat_feeder.component";
 
 void CatFeeder::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Cat Feder component...");
-  this->receive_adc_pin->setup();
+  this->receive_led_pin->setup();
   this->transmit_led_pin->setup();
   this->transmit_led_pin>digital_write(true); 
 }
