38a393a75842cc4ab3c25af1f7d67985ff1f5647
rogal138 <michal.r1993@gmail.com>
2025-07-13 22:39:55 +0200

    Fixed Iconfig

--- esphome/components/cat_feeder/__init__.py
+++ esphome/components/cat_feeder/__init__.py
@@ -26,10 +26,11 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_TRANSMIT_LED_PIN])
-    await cg.register_component(var, config)
-
     pin = await gpio_pin_expression(config[CONF_TRANSMIT_LED_PIN])
+
+    var = cg.new_Pvariable(config[CONF_ID], pin)
+    await cg.register_component(var, config)
+    
     cg.add(var.set_transmit_led_pin(pin))
     if receive_adc_pin := config.get(CONF_RECEIVE_ADC_PIN):
         pin = await gpio_pin_expression(config[CONF_RECEIVE_ADC_PIN])
